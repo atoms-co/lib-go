64e10edc6f27c36149a5b940ec04ea41f03dfe64
Henning Rohde <henning.rohde@cloudkitchens.com>
2020-12-17 12:53:58 -0800

    Add Go Zap logger adaptor
    Summary: Allows use of Zap as a backend for pkg/log.
    GitOrigin-RevId: 99d8d699787f1719ba3221888a6acb01a4c1eea2

---
+++ log/zap/zap_test.go
@@ -0,0 +1,63 @@
+package zap_test
+
+import (
+	"context"
+	"testing"
+
+	"go.atoms.co/lib/log"
+	"go.atoms.co/lib/log/zap"
+	"github.com/stretchr/testify/assert"
+	z "go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func call() {
+	log.Info(context.Background(), "bar") // update "source" test if this lines moves
+}
+
+func TestLogger(t *testing.T) {
+	entries := make(chan zapcore.Entry, 1)
+
+	zl := z.New(zapcore.RegisterHooks(z.NewExample().Core(), func(entry zapcore.Entry) error {
+		entries <- entry
+		return nil
+	}), z.AddCaller())
+
+	log.SetLogger(zap.New(zl))
+
+	t.Run("basic", func(t *testing.T) {
+		ctx := context.Background()
+
+		log.Debug(ctx, "foo")
+
+		entry := <-entries
+		assert.Equal(t, entry.Level, zapcore.DebugLevel)
+		assert.Equal(t, entry.Message, "foo")
+
+		log.Infof(ctx, "foo%v", 2)
+
+		entry = <-entries
+		assert.Equal(t, entry.Level, zapcore.InfoLevel)
+		assert.Equal(t, entry.Message, "foo2")
+
+		log.Warnf(ctx, "foo%v", 3)
+
+		entry = <-entries
+		assert.Equal(t, entry.Level, zapcore.WarnLevel)
+		assert.Equal(t, entry.Message, "foo3")
+
+		log.Errorln(ctx, "foo", "4")
+
+		entry = <-entries
+		assert.Equal(t, entry.Level, zapcore.ErrorLevel)
+		assert.Equal(t, entry.Message, "foo 4\n")
+	})
+
+	t.Run("source", func(t *testing.T) {
+		call()
+
+		entry := <-entries
+		assert.Equal(t, entry.Message, "bar")
+		assert.Equal(t, entry.Caller.FullPath(), "log/zap/zap_test.go:15")
+	})
+}
