ccf05029b26d0dd5323e3ea3894f06007e11e42c
Henning Rohde <henning.rohde@cloudkitchens.com>
2021-01-16 00:03:12 -0800

    Replace stackdriver logger with zap-based backend
    GitOrigin-RevId: 2ba71f4d61c82f29a0ff93da58e9bdbab5219c44

---
+++ log/zap/stackdriver.go
@@ -0,0 +1,79 @@
+package zap
+
+import (
+	"os"
+
+	"go.atoms.co/lib/log"
+	z "go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// NewStackdriver returns a new zap-based log.Logger with Stackdriver-compatible formatting.
+func NewStackdriver(opts ...FieldOption) log.Logger {
+	core := NewStackdriverCore(zapcore.Lock(os.Stderr))
+
+	// Enable call information. Enable zap-managed stack trace on errors and above.
+	return New(z.New(core, z.AddCaller(), z.AddStacktrace(zapcore.ErrorLevel)), opts...)
+}
+
+// NewStackdriverCore creates a new Zap core with Stackdriver formatting.
+func NewStackdriverCore(out zapcore.WriteSyncer) zapcore.Core {
+	encoder := zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+		TimeKey:       "time",
+		LevelKey:      "severity",
+		NameKey:       "logger",
+		MessageKey:    "message",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel: func(level zapcore.Level, encoder zapcore.PrimitiveArrayEncoder) {
+			encoder.AppendString(toSeverity(level))
+		},
+		EncodeTime:     zapcore.RFC3339NanoTimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+	})
+
+	core := zapcore.NewCore(encoder, out, z.DebugLevel)
+	return NewTransformCore(z.DebugLevel, core, toSourceLocation)
+}
+
+// toSourceLocation transforms Caller information to match Stackdriver expectations as defined by
+// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogEntrySourceLocation. Disables Caller.
+func toSourceLocation(entry zapcore.Entry, fields []zapcore.Field) (zapcore.Entry, []zapcore.Field) {
+	if !entry.Caller.Defined {
+		return entry, fields // nop: no caller information
+	}
+	entry.Caller.Defined = false
+
+	const (
+		key     = "logging.googleapis.com/sourceLocation"
+		fileKey = "file"
+		lineKey = "line"
+	)
+
+	source := z.Object(key, zapcore.ObjectMarshalerFunc(func(enc zapcore.ObjectEncoder) error {
+		enc.AddString(fileKey, entry.Caller.File)
+		enc.AddInt(lineKey, entry.Caller.Line)
+		return nil
+	}))
+
+	return entry, append(fields, source)
+}
+
+// toSeverity converts a zap Level to a Stackdriver-format string.
+// Based on https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+func toSeverity(l zapcore.Level) string {
+	switch l {
+	case zapcore.DebugLevel:
+		return "DEBUG"
+	case zapcore.InfoLevel:
+		return "INFO"
+	case zapcore.WarnLevel:
+		return "WARNING"
+	case zapcore.ErrorLevel:
+		return "ERROR"
+	case zapcore.DPanicLevel, zapcore.PanicLevel, zapcore.FatalLevel:
+		return "CRITICAL"
+	default:
+		return "DEFAULT"
+	}
+}
---
+++ log/zap/stackdriver_test.go
@@ -0,0 +1,56 @@
+package zap_test
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"go.atoms.co/lib/log"
+	"go.atoms.co/lib/log/zap"
+	z "go.uber.org/zap"
+	"go.uber.org/zap/zaptest"
+)
+
+func callLogger(sev log.Severity, msg string) string {
+	ctx := context.Background()
+	var buf zaptest.Buffer
+	logger := zap.New(z.New(zap.NewStackdriverCore(&buf), z.AddCaller()))
+
+	logger.Log(ctx, sev, 0, msg) // update test if this lines moves
+	_ = logger.Flush(ctx)
+
+	return buf.String()
+}
+
+type logEntry struct {
+	Timestamp string `json:"time"`
+}
+
+func TestStackdriverCore(t *testing.T) {
+	tests := []struct {
+		sev      log.Severity
+		msg      string
+		expected string
+	}{
+		{log.SevUnspecified, "foo", `{"severity":"DEBUG","time":"<TIMESTAMP>","message":"foo","logging.googleapis.com/sourceLocation":{"file":"log/zap/stackdriver_test.go","line":20}}` + "\n"},
+		{log.SevDebug, "foo", `{"severity":"DEBUG","time":"<TIMESTAMP>","message":"foo","logging.googleapis.com/sourceLocation":{"file":"log/zap/stackdriver_test.go","line":20}}` + "\n"},
+		{log.SevInfo, "foo", `{"severity":"INFO","time":"<TIMESTAMP>","message":"foo","logging.googleapis.com/sourceLocation":{"file":"log/zap/stackdriver_test.go","line":20}}` + "\n"},
+		{log.SevWarn, "foo", `{"severity":"WARNING","time":"<TIMESTAMP>","message":"foo","logging.googleapis.com/sourceLocation":{"file":"log/zap/stackdriver_test.go","line":20}}` + "\n"},
+		{log.SevError, "foo", `{"severity":"ERROR","time":"<TIMESTAMP>","message":"foo","logging.googleapis.com/sourceLocation":{"file":"log/zap/stackdriver_test.go","line":20}}` + "\n"},
+		{log.SevFatal, "foo", `{"severity":"CRITICAL","time":"<TIMESTAMP>","message":"foo","logging.googleapis.com/sourceLocation":{"file":"log/zap/stackdriver_test.go","line":20}}` + "\n"},
+	}
+
+	for _, test := range tests {
+		output := callLogger(test.sev, test.msg)
+
+		// We can't control zap time, so we replace the expected TIMESTAMP with the actual zap timestamp.
+		var entry logEntry
+		_ = json.Unmarshal([]byte(output), &entry)
+		expected := strings.ReplaceAll(test.expected, "<TIMESTAMP>", entry.Timestamp)
+
+		if output != expected {
+			t.Errorf("Incorrect message:\n(%v), want:\n(%v)", output, expected)
+		}
+	}
+}
