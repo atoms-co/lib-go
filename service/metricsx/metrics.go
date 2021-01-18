7de3c3682fa77df818ce8b392381ec2eb18ae3c6
Henning Rohde <henning.rohde@cloudkitchens.com>
2021-01-17 22:00:56 -0800

    Consolidate Go metrics initialization
    GitOrigin-RevId: 79c603a75437d705574c88609059bccd4d733633

---
+++ service/metricsx/metrics.go
@@ -0,0 +1,37 @@
+package metricsx
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	promExporter "contrib.go.opencensus.io/exporter/prometheus"
+	"go.atoms.co/lib/log"
+	"go.atoms.co/lib/metrics"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+var (
+	port = flag.Int("prometheus_port", 9090, "Prometheus metrics exporter port")
+)
+
+// Init initializes metrics with the givne application name and exports them on port 9090.
+func Init(ctx context.Context, application string) {
+	log.Infof(ctx, "Initializing prometheus metrics on :%v", *port)
+
+	pe, err := promExporter.NewExporter(promExporter.Options{})
+	if err != nil {
+		log.Exitf(ctx, "Failed to create Prometheus exporter: %v", err)
+	}
+
+	r := mux.NewRouter()
+	r.Handle("/metrics", pe)
+	go func() {
+		log.Fatalf(ctx, "Metrics server exited: %v", http.ListenAndServe(fmt.Sprintf(":%v", *port), r))
+	}()
+
+	if err := metrics.Init(application); err != nil {
+		log.Exitf(ctx, "Failed to initialize metric: %v", err)
+	}
+}
