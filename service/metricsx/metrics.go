791fa3a30065fcc510d26966035d25420f9e434b
Jordan Hurwitz <jordan.hurwitz@cloudkitchens.com>
2021-06-29 19:21:49 +0000

    [lib/go] Add shutdown to metricsx
    Summary: Adds shutdown function to metricsx Init in case a use wants to gracefully close the metrics server
    GitOrigin-RevId: cb9087f29a748ce5928b3a39c49f7bc186cfd091

--- service/metricsx/metrics.go
+++ service/metricsx/metrics.go
@@ -6,17 +6,19 @@ import (
 	"fmt"
 
 	promExporter "contrib.go.opencensus.io/exporter/prometheus"
-	"go.cloudkitchens.org/lib/log"
-	"go.cloudkitchens.org/lib/metrics"
 	"github.com/gorilla/mux"
 	"net/http"
+
+	"go.cloudkitchens.org/lib/log"
+	"go.cloudkitchens.org/lib/metrics"
 )
 
 var (
 	port = flag.Int("prometheus_port", 9090, "Prometheus metrics exporter port")
+	srv  = &http.Server{}
 )
 
-// Init initializes metrics with the givne application name and exports them on port 9090.
+// Init initializes metrics with the given application name and exports them on port 9090.
 func Init(ctx context.Context, application string) {
 	log.Infof(ctx, "Initializing prometheus metrics on :%v", *port)
 
@@ -27,11 +29,18 @@ func Init(ctx context.Context, application string) {
 
 	r := mux.NewRouter()
 	r.Handle("/metrics", pe)
+	srv = &http.Server{Addr: fmt.Sprintf(":%v", *port), Handler: r}
 	go func() {
-		log.Fatalf(ctx, "Metrics server exited: %v", http.ListenAndServe(fmt.Sprintf(":%v", *port), r))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf(ctx, "Metrics server exited unexpectedly: %v", err)
+		}
 	}()
 
 	if err := metrics.Init(application); err != nil {
 		log.Exitf(ctx, "Failed to initialize metric: %v", err)
 	}
+}
+
+func Shutdown(ctx context.Context) error {
+	return srv.Shutdown(ctx)
 }
