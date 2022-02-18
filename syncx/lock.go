ae93b75e98279739da0d7eca205d6aa1590d8735
Henning Rohde <henning.rohde@cloudkitchens.com>
2022-02-18 23:30:11 +0000

    Add syncx.Lock
    GitOrigin-RevId: fe0f4fca570d6d94d973867caa0d0cc0c5628e6a

---
+++ syncx/lock.go
@@ -0,0 +1,73 @@
+package syncx
+
+import (
+	"go.atoms.co/lib/iox"
+	"go.atoms.co/lib/mathx"
+	"go.uber.org/atomic"
+)
+
+// Lock is a chan-based closeable semaphore with N concurrent locks granted. If N=1, it acts as a mutex.
+// Using channels allows callers to block and be interrupted by other events via select.
+type Lock struct {
+	iox.AsyncCloser
+
+	guard   chan bool    // chan of lock grants
+	pending atomic.Int64 // #unprocessed unlocks
+	pulse   chan bool    // 1-buf trigger to process unlock
+}
+
+func NewLock(n int) *Lock {
+	n = mathx.MaxInt(1, n)
+	ret := &Lock{
+		AsyncCloser: iox.NewAsyncCloser(),
+		guard:       make(chan bool, n),
+		pulse:       make(chan bool, 1),
+	}
+	for i := 0; i < n; i++ {
+		ret.Unlock()
+	}
+	go ret.process()
+
+	return ret
+}
+
+// TryLock returns the grant chan. If the result is true the lock is granted. If false, the
+// lock has been closed (and hence a chan read returns the default value).
+func (l *Lock) TryLock() <-chan bool {
+	return l.guard
+}
+
+// Unlock frees a (previously held) lock. If called too many times, it cannot exceed the initial
+// limit. There is a slight delay for the lock to be re-enqueued, so a Unlock + default-select
+// TryLock may fail to acquire the released lock.
+func (l *Lock) Unlock() {
+	l.pending.Inc()
+
+	select {
+	case l.pulse <- true:
+	default:
+	}
+}
+
+func (l *Lock) process() {
+	defer close(l.guard)
+
+	for {
+		select {
+		case <-l.Closed():
+			// Close guard and stop processing unlocks.
+			return
+
+		case <-l.pulse:
+			n := int(l.pending.Load())
+			for i := 0; i < n; i++ {
+				l.pending.Dec()
+
+				select {
+				case l.guard <- true:
+				default:
+				}
+			}
+		}
+	}
+}
