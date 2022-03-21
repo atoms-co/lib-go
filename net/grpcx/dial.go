341d5ad41a225b2366f785463d38a82fdda5c442
rishi.marwah <rishi.marwah@cloudkitchens.com>
2022-03-21 23:56:02 +0000

    Add tracing to grpcx util dial
    Summary: Automatically trace client calls with css grpcx util function
    GitOrigin-RevId: 0eac0d166c8dc7b693c1ebca458c41619e68f6f8

--- net/grpcx/dial.go
+++ net/grpcx/dial.go
@@ -5,16 +5,17 @@ import (
 	"context"
 	"time"
 
+	"go.cloudkitchens.org/lib/statshandlerx"
 	"github.com/pkg/errors"
 	"google.golang.org/grpc"
 )
 
-// Dial makes a blocking grpc dial with a timeout.
+// Dial makes a blocking grpc dial with a timeout & tracing.
 func Dial(ctx context.Context, endpoint string, timeout time.Duration, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	cc, err := grpc.DialContext(ctx, endpoint, append(opts, grpc.WithBlock())...)
+	cc, err := grpc.DialContext(ctx, endpoint, append(opts, grpc.WithBlock(), statshandlerx.WithClientGRPCStatsHandler())...)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to dial server at %v", endpoint)
 	}
