df1343d76aced5097e3b320001d0e81367b764c6
Henning Rohde <henning.rohde@cloudkitchens.com>
2022-08-19 21:05:03 +0000

    PORC: add test support + minor tweaks
    Summary:
    Don't info from pops proxy
    Ready facilities with UNKNOWN printers
    Don't bind printer manually -- let POPS do it
    GitOrigin-RevId: 6e622a31071c7d52d8b797e8cc5c122a812db400

--- chanx/chan.go
+++ chanx/chan.go
@@ -4,6 +4,7 @@ package chanx
 import (
 	"go.cloudkitchens.org/lib/mathx"
 	"sync"
+	"time"
 )
 
 // NewFixed returns a new closed chan with the given elements.
@@ -40,4 +41,34 @@ func Process[T any](in <-chan T, n int, fn func(t T)) {
 		}()
 	}
 	wg.Wait()
+}
+
+// TryRead reads an element, waiting up to the given timeout, Returns false otherwise.
+func TryRead[T any](ch <-chan T, timeout time.Duration) (T, bool) {
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+
+	select {
+	case elm, ok := <-ch:
+		if ok {
+			return elm, true
+		}
+	case <-timer.C:
+	}
+
+	var t T
+	return t, false
+}
+
+// TryWrite writes an element, waiting up to the given timeout, Returns false otherwise.
+func TryWrite[T any](ch chan<- T, t T, timeout time.Duration) bool {
+	timer := time.NewTimer(timeout)
+	defer timer.Stop()
+
+	select {
+	case ch <- t:
+		return true
+	case <-timer.C:
+		return false
+	}
 }
