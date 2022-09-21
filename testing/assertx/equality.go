c25a43cd0982bf4271cfe454c08d7d1ea51340ae
Henning Rohde <henning.rohde@cloudkitchens.com>
2022-09-21 03:57:08 +0000

    PORC: fix customer ticket station selection
    GitOrigin-RevId: 0e1ed9e89ce08fd51a90c96fcc1a004a9bb8cce2

--- testing/assertx/equality.go
+++ testing/assertx/equality.go
@@ -5,8 +5,14 @@ import (
 	"testing"
 )
 
-// Equal is a typed convenience wrapper over assert.Equals to make constants the correct type. Also
-// uses the idoimatic "have before want" parameter ordering.
+// Equal is a typed convenience wrapper over assert.Equal to make constants the correct type. Also
+// uses the idiomatic "have before want" parameter ordering.
 func Equal[T any](t *testing.T, actual, expected T, args ...any) {
 	assert.Equal(t, expected, actual, args...)
+}
+
+// NotEqual is a typed convenience wrapper over assert.NotEqual to make constants the correct type. Also
+// uses the idiomatic "have before want" parameter ordering.
+func NotEqual[T any](t *testing.T, actual, expected T, args ...any) {
+	assert.NotEqual(t, expected, actual, args...)
 }
