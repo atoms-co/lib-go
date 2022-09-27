58f3f739a319fce26f81ee020e843a01d94e78a3
Kris Raney <kristopher.raney@cloudkitchens.com>
2022-09-27 20:25:49 +0000

    Add container.Heap
    GitOrigin-RevId: d158ee35cb47380c9aed9f000b449d0d8f7fb533

---
+++ container/heap.go
@@ -0,0 +1,70 @@
+package container
+
+import "container/heap"
+
+type heapStore[T any] struct {
+	data []T
+	comp func(a, b T) bool
+}
+
+func (hs *heapStore[T]) Len() int {
+	return len(hs.data)
+}
+
+func (hs *heapStore[T]) Less(i, j int) bool {
+	return hs.comp(hs.data[i], hs.data[j])
+}
+
+func (hs *heapStore[T]) Swap(i, j int) {
+	tmp := hs.data[i]
+	hs.data[i] = hs.data[j]
+	hs.data[j] = tmp
+}
+
+func (hs *heapStore[T]) Push(x any) {
+	t, ok := x.(T)
+	if !ok {
+		panic("Unexpected type passed to Push")
+	}
+	hs.data = append(hs.data, t)
+}
+
+func (hs *heapStore[T]) Pop() any {
+	tmp := hs.data[len(hs.data)-1]
+	hs.data = hs.data[:len(hs.data)-1]
+	return tmp
+}
+
+type Heap[T any] struct {
+	store *heapStore[T]
+}
+
+func NewHeap[T any](comp func(a, b T) bool) *Heap[T] {
+	return &Heap[T]{
+		store: &heapStore[T]{
+			data: make([]T, 0),
+			comp: comp,
+		},
+	}
+}
+
+func (h *Heap[T]) Push(t T) {
+	heap.Push(h.store, t)
+}
+
+func (h *Heap[T]) Pop() T {
+	x := heap.Pop(h.store)
+	ret, ok := x.(T)
+	if !ok {
+		panic("Unexpected type returned by Pop")
+	}
+	return ret
+}
+
+func (h *Heap[T]) Peek() T {
+	return h.store.data[0]
+}
+
+func (h *Heap[T]) Len() int {
+	return h.store.Len()
+}
---
+++ container/heap_test.go
@@ -0,0 +1,27 @@
+package container
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeapInt(t *testing.T) {
+	heap := NewHeap[int](func(a, b int) bool { return a < b })
+	values := []int{6, 5, 4, 8, 9, 10, 13, 12, 11, 7}
+
+	// Push
+	for _, v := range values {
+		heap.Push(v)
+	}
+	sort.Slice(values, func(i, j int) bool {
+		return values[i] < values[j]
+	})
+	i := 0
+	for heap.Len() > 0 {
+		val := heap.Pop()
+		require.Equal(t, values[i], val, "item at index %v", i)
+		i++
+	}
+}
