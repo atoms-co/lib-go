05604df7c09bd84e2d4a5a57e3cfea4301c1d0de
daniel.fagnan <daniel.fagnan@cloudkitchens.com>
2022-11-11 22:20:14 +0000

    New uuid split method
    GitOrigin-RevId: 196121a1380d2cfdb846bf97d261a92aa70cbe62

--- uuidx/range.go
+++ uuidx/range.go
@@ -4,13 +4,20 @@ package uuidx
 import (
 	"bytes"
 	"fmt"
+	"math"
 	"math/big"
 
+	"go.atoms.co/lib/css/slices"
 	"github.com/google/uuid"
 )
 
+var (
+	Min = uuid.Nil // Provide Min to be consistent with Max
+	Max = uuid.MustParse("ffffffff-ffff-ffff-ffff-ffffffffffff")
+)
+
 // Domain is a the full UUID range.
-var Domain = Range{from: uuid.Nil, to: uuid.MustParse("ffffffff-ffff-ffff-ffff-ffffffffffff")}
+var Domain = Range{from: uuid.Nil, to: Max}
 
 // Range represents a half-open UUID range [from, to).
 type Range struct {
@@ -78,6 +85,92 @@ func (s Range) String() string {
 	return fmt.Sprintf("[%s;%s)", s.from.String(), s.to.String())
 }
 
+// SplitByWeights shards a UUID range by a set of weights that determine the size of each shard.
+// For example, weights of '(50, 50)' would give 2 shards of equal size whereas weights of (50, 25, 10, 15) would give
+// 4 shards of various different sizes.
+func SplitByWeights(s Range, weights ...int) ([]Range, error) {
+	var (
+		ranges       = make([]Range, len(weights))
+		start        = s.From()
+		end          = s.To()
+		tokenRange   = big.NewInt(0)
+		total        = big.NewInt(0)
+		weightsTotal = 0
+	)
+
+	total = total.Sub(
+		big.NewInt(0).SetBytes(end[:]),
+		big.NewInt(0).SetBytes(start[:]),
+	).
+		Add(total, big.NewInt(1))
+
+	// Keep track of the original position of each weight so that
+	// weights that are 0 can remain in-place without affecting
+	// the UUID splitting logic.
+	originalPos := make(map[int]int)
+
+	// Calculate how many UUIDs should be allocate to each shard based on
+	// the weights given and the total number of UUIDs available in the current
+	// range.
+	uuidsPerWeight := make([]*big.Int, 0)
+	for i, weight := range weights {
+		// Any weights that are zero require an emtpy range to be created
+		// as the ordering needs to be preserved.
+		if weight == 0 {
+			ranges[i] = Range{
+				from: Min,
+				to:   Min,
+			}
+
+			continue
+		}
+
+		// Number of UUIDs allocated per shard = Total / weight * 100
+		div := big.NewInt(0).Mul(total, big.NewInt(int64(weight)))
+		div.Div(div, big.NewInt(100))
+
+		uuidsPerWeight = append(uuidsPerWeight, div)
+
+		// Keep track of the index that was just inserted as zero weights are
+		// not inserted into `uuidsPerWeight` but still need to be tracked.
+		originalPos[len(uuidsPerWeight)-1] = i
+
+		// Sum all the weights to ensure we allocate 100%.
+		weightsTotal += weight
+	}
+
+	if weightsTotal != 100 {
+		return nil, fmt.Errorf("expected the weights to sum to 100 but got %v", weightsTotal)
+	}
+
+	var to uuid.UUID
+	var err error
+	for i, weightedPartSize := range uuidsPerWeight {
+		// Ensure the last index always allocates up to ffff-fffff..
+		if i == len(uuidsPerWeight)-1 {
+			to = end
+		} else {
+			tokenRange.Add(weightedPartSize, big.NewInt(0).SetBytes(start[:]))
+
+			tokenRangeBytes := make([]byte, 16)
+			if to, err = uuid.FromBytes(tokenRange.FillBytes(tokenRangeBytes)); err != nil {
+				return []Range{}, fmt.Errorf("partition range: %v", err)
+			}
+		}
+
+		split, err := NewRange(start, to)
+		if err != nil {
+			return nil, fmt.Errorf("range [%s-%s] is invalid: %s", start, to, err)
+		}
+
+		pos := originalPos[i]
+		ranges[pos] = split
+		start = to
+	}
+
+	return ranges, nil
+}
+
 // Split uniformly splits the given range into N sub-ranges of equal size.
 func Split(s Range, numPartitions int) ([]Range, error) {
 	start := s.From()
@@ -141,4 +234,78 @@ func Inc(n uuid.UUID) uuid.UUID {
 	next := big.NewInt(0).Add(big.NewInt(0).SetBytes(n[:]), big.NewInt(1))
 	ret, _ := uuid.FromBytes(next.FillBytes(make([]byte, 16)))
 	return ret
+}
+
+// IsSerializable verifies whether a set of ranges can be serialized (i.e., strictly ordered)
+// and contains the full "Domain" UUID keyspace.
+//
+// The given `ranges` don't need to be ordered as long as the totality can be ordered.
+func IsRangeSerializable(ranges ...Range) bool {
+	var (
+		startRanges = make(map[uuid.UUID]Range)
+		start       = uuid.Nil
+		end         = Max
+	)
+
+	for _, r := range ranges {
+		// Skip any zeroed ranges
+		if r.from == Min && r.to == Min {
+			continue
+		}
+
+		startRanges[r.From()] = r
+	}
+
+	for start != end {
+		next, ok := startRanges[start]
+		if !ok {
+			return false
+		}
+
+		start = next.To()
+		if start == uuid.Nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+// RangeDistributions takes a list of UUID ranges and calculates the distribution %
+// based on how many UUIDs are allocated in each. This is typically used in conjunction with
+// the `SplitByWeights` methods that create uneven allocations.
+//
+// The map that is returned is keyed by the index of the `ranges` that were inputed and the
+// value is a weighted percentage that the given range index has allocated.
+func RangeDistributions(ranges ...Range) map[int]int64 {
+	// Not all situations have a full UUID range so need to collect the total amount of UUIDs
+	// that can be allocated across the set of ranges given.
+	totalSize := slices.Fold(ranges, big.NewInt(0), func(total *big.Int, r Range) *big.Int {
+		return total.Add(total, r.Size())
+	})
+
+	// Map key is the index of the `ranges` element.
+	allocs := make(map[int]int64, len(ranges))
+
+	for index, rng := range ranges {
+		totalSizeF := big.NewFloat(0).SetInt(totalSize)
+		sizeF := big.NewFloat(0).SetInt(rng.Size())
+
+		// Handle zero weights
+		if rng.Size().Cmp(big.NewInt(0)) == 0 {
+			allocs[index] = 0
+			continue
+		}
+
+		// Size of allocation / Total size of UUID ranges
+		res := sizeF.Quo(sizeF, totalSizeF)
+		p, _ := res.Float64()
+
+		// Round is needed for some edge cases where values are 0.9999
+		//
+		// Normalized Percentage = Percentage of UUID Allocations * 100
+		allocs[index] = int64(math.Round(p * 100))
+	}
+
+	return allocs
 }
--- uuidx/range_test.go
+++ uuidx/range_test.go
@@ -1,6 +1,7 @@
 package uuidx_test
 
 import (
+	"fmt"
 	"testing"
 
 	"go.atoms.co/lib/uuidx"
@@ -8,6 +9,70 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func FuzzTestSplitByWeights(f *testing.F) {
+	f.Add(50, 40, 10)
+
+	f.Fuzz(func(t *testing.T, a, b, c int) {
+		weights := []int{a, b, c}
+
+		if a+b+c != 100 || a < 0 || b < 0 || c < 0 {
+			t.SkipNow()
+			return
+		}
+
+		shards, err := uuidx.SplitByWeights(uuidx.Domain, weights...)
+		assert.NoError(t, err)
+		assert.Equal(t, len(weights), len(shards))
+		assert.True(t, uuidx.IsRangeSerializable(shards...))
+
+		distributions := uuidx.RangeDistributions(shards...)
+		for i, distribution := range distributions {
+			assert.Equal(t, weights[i], int(distribution), fmt.Sprintf("actual distribution: %+v | expected: %+v", distributions, weights))
+		}
+	})
+}
+
+func TestSplitByWeights(t *testing.T) {
+	tests := []struct {
+		weights       []int
+		expectedError bool
+	}{
+		{weights: []int{100}},
+		{weights: []int{50, 50}},
+		{weights: []int{25, 25, 25, 25}},
+		{weights: []int{50, 25, 25}},
+		{weights: []int{25, 25, 50}},
+		{weights: []int{10, 90}},
+		{weights: []int{90, 10}},
+		{weights: []int{99, 1}},
+		{weights: []int{95, 5}},
+		{weights: []int{90, 5, 5}},
+		{weights: []int{23, 44, 33}},
+		{weights: []int{90, 0, 10}},
+		{weights: []int{58, 26, 16}},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(fmt.Sprintf("%v", test.weights), func(t *testing.T) {
+			shards, err := uuidx.SplitByWeights(uuidx.Domain, test.weights...)
+			if test.expectedError && err == nil {
+				t.Fatal("expected error but got <nil>")
+			} else if !test.expectedError && err != nil {
+				t.Fatalf("expected no error but got %s", err)
+			}
+
+			assert.Equal(t, len(test.weights), len(shards))
+			assert.True(t, uuidx.IsRangeSerializable(shards...))
+
+			distributions := uuidx.RangeDistributions(shards...)
+			for i, distribution := range distributions {
+				assert.Equal(t, test.weights[i], int(distribution), fmt.Sprintf("actual distribution: %+v | expected: %+v", distributions, test.weights))
+			}
+		})
+	}
+}
 
 func TestSplit(t *testing.T) {
 	from := uuid.MustParse("80000000-0000-0000-0000-000000000000")
