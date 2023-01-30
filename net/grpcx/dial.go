9e38a4dd920647982e23524bfbe72b160616da51
Henning Rohde <henning.rohde@cloudkitchens.com>
2023-01-30 19:24:32 +0000

    KEQ2: add basic wds and range plumbing
    Summary:
    * Added wds dispatcher (from cp2.1)
    * Added errors and metrics
    * Add tentative structure for wds and range.
    GitOrigin-RevId: 5182f8e87a175699b9ec10a6c0487b6aaf49031a

--- net/grpcx/dial.go
+++ net/grpcx/dial.go
@@ -1,8 +1,9 @@
-// Package grpx contain utilities for working with grpc.
+// Package grpcx contain utilities for working with grpc.
 package grpcx
 
 import (
 	"context"
+	"google.golang.org/grpc/credentials/insecure"
 	"time"
 
 	"go.cloudkitchens.org/lib/statshandlerx"
@@ -10,12 +11,23 @@ import (
 	"google.golang.org/grpc"
 )
 
+// WithInsecure returns an insecure transport credential option. Convenience replacement for the deprecated
+// grpc.WithInsecure.
+func WithInsecure() grpc.DialOption {
+	return grpc.WithTransportCredentials(insecure.NewCredentials())
+}
+
 // Dial makes a blocking grpc dial with a timeout & tracing.
 func Dial(ctx context.Context, endpoint string, timeout time.Duration, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	cc, err := grpc.DialContext(ctx, endpoint, append(opts, grpc.WithBlock(), statshandlerx.WithClientGRPCStatsHandler())...)
+	return DialNonBlocking(ctx, endpoint, append(opts, grpc.WithBlock())...)
+}
+
+// DialNonBlocking makes a non-blocking grpc dial with tracing.
+func DialNonBlocking(ctx context.Context, endpoint string, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	cc, err := grpc.DialContext(ctx, endpoint, append(opts, statshandlerx.WithClientGRPCStatsHandler())...)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to dial server at %v", endpoint)
 	}
