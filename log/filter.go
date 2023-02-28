2b394291a54c3c4bd38bd788b2f6d62121d9f3ee
Henning Rohde <henning.rohde@cloudkitchens.com>
2023-02-28 03:58:09 +0000

    KEQ2: add single node setup
    Summary:
    Docker compose setup runs in limited form:
    
    ```
    $ keq2ctl --insecure -e :8100 enqueue tenant topic 015392A8-199E-4E08-AD9B-BD06D429EAB8 bar
    $ keq2ctl --insecure -e :8100 enqueue tenant topic 015392A8-199E-4E08-AD9B-BD06D429EAB8 bar
    $ keq2ctl --insecure -e :8100 info tenant topic 015392A8-199E-4E08-AD9B-BD06D429EAB0
    Error: rpc error: code = NotFound desc = not found
    rpc error: code = NotFound desc = not found
    $ keq2ctl --insecure -e :8100 info tenant topic 015392A8-199E-4E08-AD9B-BD06D429EAB8
    {
      "range": {
        "range": {
          "id": "3bf507ee-e1c9-4df1-833b-1cbfbed62acf",
          "shard": {
            "topic": {
              "tenant": "tenant",
              "topic": "topic"
            },
            "region": "us-west2",
            "to": "ffffffff-ffff-ffff-ffff-ffffffffffff"
          },
          "location": {
            "region": "us-west2",
            "node": "instance"
          },
          "created": "2023-02-28T02:07:42.666910Z"
        }
      },
      "queue": {
        "queue": {
          "topic": {
            "tenant": "tenant",
            "topic": "topic"
          },
          "alignment": {},
          "key": "015392a8-199e-4e08-ad9b-bd06d429eab8"
        },
        "metadata": {
          "first": {
            "seqno": "1",
            "timestamp": "2023-02-28T02:07:49.231453Z"
          },
          "last": {
            "seqno": "2",
            "timestamp": "2023-02-28T02:07:52.784298Z"
          }
        },
        "subscriptions": [
          {
            "subscription": "foo",
            "cursor": {},
            "activated": {
              "seqno": "1"
            }
          }
        ]
      }
    }
    
    ```
    
    GitOrigin-RevId: d5e8e1fb17e24d32e38ca38a499bba52094ed2c0

--- log/filter.go
+++ log/filter.go
@@ -2,25 +2,43 @@ package log
 
 import "context"
 
+const logCutoffKey logCtxKeyType = "log_cutoff"
+
+// WithCutoff returns a context with the given severity cutoff, used by the Filter logger. The severity
+// will take effect where the context flows, incl. helper functions and libraries that may be called in
+// various contexts. The contextual setting thus allows a precise filtering than the class-scoped filtering
+// approach typically used in OO-centric languages like Java.
+func WithCutoff(ctx context.Context, cutoff Severity) context.Context {
+	return context.WithValue(ctx, logCutoffKey, cutoff)
+}
+
+// CutoffValue returns an effective severity set by WithCutoff, if present.
+func CutoffValue(ctx context.Context) (Severity, bool) {
+	sev, ok := ctx.Value(logCutoffKey).(Severity)
+	return sev, ok
+}
+
 type filter struct {
 	l      Logger
 	cutoff Severity
 }
 
-// Filter is a wrapper that drops any logs below the given severity, such as debug or info.
+// Filter is a wrapper that drops any logs below the given severity, such as debug or info. The effective severity
+// may also be passed in the context to disable/enable noisy parts of a program, notably external libraries.
 func Filter(l Logger, cutoff Severity) Logger {
 	if l == nil {
 		panic("nil logger")
-	}
-	if cutoff <= SevUnspecified {
-		return l // nop: no filtering needed
 	}
 	return &filter{l: l, cutoff: cutoff}
 }
 
 func (f *filter) Log(ctx context.Context, sev Severity, calldepth int, msg string) {
-	if sev < f.cutoff {
-		return
+	if s, ok := CutoffValue(ctx); ok {
+		if sev < s {
+			return // omit: cutoff override
+		}
+	} else if sev < f.cutoff {
+		return // omit: default cutoff, no context override
 	}
 	f.l.Log(ctx, sev, calldepth+1, msg) // +1 for this frame
 }
