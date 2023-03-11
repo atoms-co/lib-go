478897def15f062a56c5f6e8d072c0787f47e114
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2023-03-11 02:23:37 +0000

    Add assertx.EqualProtobuf
    GitOrigin-RevId: 6604d8fd39244ea8394b61f4a6497daa2b9ee630

--- testing/assertx/equality.go
+++ testing/assertx/equality.go
@@ -1,6 +1,7 @@
 package assertx
 
 import (
+	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -8,11 +9,18 @@ import (
 // Equal is a typed convenience wrapper over assert.Equal to make constants the correct type. Also
 // uses the idiomatic "have before want" parameter ordering.
 func Equal[T any](t *testing.T, actual, expected T, args ...any) {
+	t.Helper()
 	assert.Equal(t, expected, actual, args...)
 }
 
 // NotEqual is a typed convenience wrapper over assert.NotEqual to make constants the correct type. Also
 // uses the idiomatic "have before want" parameter ordering.
 func NotEqual[T any](t *testing.T, actual, expected T, args ...any) {
+	t.Helper()
 	assert.NotEqual(t, expected, actual, args...)
+}
+
+func EqualProtobuf(t *testing.T, actual, expected proto.Message, args ...any) {
+	t.Helper()
+	assert.Equal(t, proto.MarshalTextString(actual), proto.MarshalTextString(expected), args)
 }
