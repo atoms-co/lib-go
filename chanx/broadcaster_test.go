395f4c58f61358a481d5a0a797f56ad88ec0c1f9
jordan.hurwitz <jordan.hurwitz@cloudkitchens.com>
2023-03-17 17:28:56 +0000

    Add chanx.Broadcaster
    GitOrigin-RevId: a718d6cc35064a99e97555ed23a283c39b6fbda1

---
+++ chanx/broadcaster_test.go
@@ -0,0 +1,133 @@
+package chanx_test
+
+import (
+	"context"
+	"go.atoms.co/lib/chanx"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestBroadcaster_SingleConsumer(t *testing.T) {
+	ctx := context.Background()
+
+	b := chanx.NewBroadcaster[int](ctx)
+	defer b.Close()
+
+	// (1) Connect consumer
+	out, quit := b.Connect(ctx)
+
+	// (2) Forward channel
+	in := make(chan int, 1)
+	b.Forward(ctx, in)
+
+	// (3) Read first update
+	in <- 1
+	m := read(t, out)
+	assert.Equal(t, 1, m)
+
+	// (4) Disconnect
+	quit.Close()
+	time.Sleep(100 * time.Millisecond)
+
+	// (5) Don't read update
+	in <- 2
+	dontRead(t, out)
+}
+
+func TestBroadcaster_MultiConsumer(t *testing.T) {
+	ctx := context.Background()
+
+	b := chanx.NewBroadcaster[int](ctx)
+	defer b.Close()
+
+	// (1) Connect consumer 1
+	out1, quit1 := b.Connect(ctx)
+
+	// (2) Forward channel
+	in := make(chan int, 1)
+	b.Forward(ctx, in)
+
+	// (3) Read first update
+	in <- 1
+	m1 := read(t, out1)
+	assert.Equal(t, 1, m1)
+
+	// (4) Connect consumer 2, receives first message
+	out2, quit2 := b.Connect(ctx)
+	defer quit2.Close()
+	m2 := read(t, out2)
+	assert.Equal(t, 1, m2)
+
+	// (5) Disconnect first consumer
+	quit1.Close()
+	time.Sleep(100 * time.Millisecond)
+
+	// (6) First consumer doesn't read update
+	in <- 2
+	dontRead(t, out1)
+	m3 := read(t, out2)
+	assert.Equal(t, 2, m3)
+}
+
+func TestBroadcaster_ChangeForward(t *testing.T) {
+	ctx := context.Background()
+
+	b := chanx.NewBroadcaster[int](ctx)
+	defer b.Close()
+
+	// (1) Connect consumer
+	out, quit := b.Connect(ctx)
+
+	// (2) Forward channel
+	in := make(chan int, 1)
+	b.Forward(ctx, in)
+
+	// (3) Read first update
+	in <- 1
+	m := read(t, out)
+	assert.Equal(t, 1, m)
+
+	// (4) Forward new channel
+	in2 := make(chan int, 1)
+	b.Forward(ctx, in2)
+
+	// (5) Don't read update from first channel
+	in <- 2
+	dontRead(t, out)
+
+	// (6) Read update from second channel
+	in2 <- 3
+	m = read(t, out)
+	assert.Equal(t, 3, m)
+
+	// (7) Disconnect
+	quit.Close()
+	time.Sleep(100 * time.Millisecond)
+
+	// (8) Don't read update
+	in2 <- 2
+	dontRead(t, out)
+}
+
+func read(t *testing.T, in <-chan int) int {
+	t.Helper()
+	select {
+	case m := <-in:
+		return m
+	case <-time.After(1 * time.Second):
+		t.Fatalf("failed to read message")
+		return -1
+	}
+}
+
+func dontRead(t *testing.T, in <-chan int) {
+	t.Helper()
+	select {
+	case m, ok := <-in:
+		if ok {
+			t.Fatalf("unexpected message, %v", m)
+		}
+	case <-time.After(1 * time.Second):
+	}
+}
