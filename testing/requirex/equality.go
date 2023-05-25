9af9381e23a5bf8d45cc9df0c2b9100b89b46bbe
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2023-05-25 23:29:26 +0000

    [KEQ2] Validate topics and subscriptions in keq2-topics
    GitOrigin-RevId: d8ae5dcf2509a62d001e38d53be587263f4b6c05

---
+++ testing/requirex/equality.go
@@ -0,0 +1,26 @@
+package requirex
+
+import (
+	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// Equal is a typed convenience wrapper over require.Equal to make constants the correct type. Also
+// uses the idiomatic "have before want" parameter ordering.
+func Equal[T any](t *testing.T, actual, expected T, args ...any) {
+	t.Helper()
+	require.Equal(t, expected, actual, args...)
+}
+
+// NotEqual is a typed convenience wrapper over require.NotEqual to make constants the correct type. Also
+// uses the idiomatic "have before want" parameter ordering.
+func NotEqual[T any](t *testing.T, actual, expected T, args ...any) {
+	t.Helper()
+	require.NotEqual(t, expected, actual, args...)
+}
+
+func EqualProtobuf[T proto.Message](t *testing.T, actual, expected T, args ...any) {
+	t.Helper()
+	require.Equal(t, proto.MarshalTextString(actual), proto.MarshalTextString(expected), args)
+}
