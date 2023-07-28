8d7cc27fe0f49a895eec569ec3711665d2d3b3e0
jianwu chen <jianwu.chen@cloudkitchens.com>
2023-07-28 22:52:28 +0000

    [libs/go/tracing]  add propagated fiberWorkflow it to log and metrics for grpc tracing library.
    Summary:
    Scope
    - Updated grpc metrics to emit `fiber_workflow_grpc_count`
    - Updated grpc tracing to inject fiber_workflow related data to log context, so that will appear in the log
    - Extract log_test_recorder as a library so that other application can reuse it to verify log output.
    - Added metrics_test_util as a test util to verify metrics instrumentation logic.
    - Updated unit test/integration test to cover the new logic
    - Updated go sample app add fiber workflow
    - Add README.md for go sample app
    GitOrigin-RevId: 58a02fd96fdef2bdf793dd77cae599e63e7b0c2f

---
+++ log/testing/log_test_recorder.go
@@ -0,0 +1,37 @@
+package log_testing
+
+import (
+	"context"
+
+	"go.cloudkitchens.org/lib/log"
+)
+
+type Call struct {
+	Sev       log.Severity
+	Calldepth int
+	Fields    []log.Field
+	Msg       string
+}
+
+// TestRecorder is a simple test Logger that records the invocations. This is useful for testing to verify
+// logged data is as expected
+type TestRecorder struct {
+	Calls   []Call
+	Flushes int
+}
+
+func (l *TestRecorder) Log(ctx context.Context, sev log.Severity, calldepth int, msg string) {
+	l.Calls = append(l.Calls, Call{Sev: sev, Calldepth: calldepth, Msg: msg, Fields: log.FromContext(ctx)})
+}
+
+func (l *TestRecorder) Flush(ctx context.Context) error {
+	l.Flushes++
+	return nil
+}
+
+func (l *TestRecorder) Reset() ([]Call, int) {
+	calls, flushes := l.Calls, l.Flushes
+	l.Calls = nil
+	l.Flushes = 0
+	return calls, flushes
+}
---
+++ metrics/testing/metrics_test_util.go
@@ -0,0 +1,89 @@
+package metrics_testing
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"go.opencensus.io/metric/metricdata"
+	"go.opencensus.io/metric/metricproducer"
+)
+
+// MetricsToString convert metrics to string for specified metrics names. For testing only.
+func MetricsToString(names ...string) string {
+	var sb strings.Builder
+	for _, name := range names {
+		metric := FindMetrics(name)
+		if metric != nil {
+			sb.WriteString(MetricToString(metric))
+		}
+	}
+	return sb.String()
+}
+
+// FindMetrics find metrics by name. For testing only.
+func FindMetrics(name string) *metricdata.Metric {
+	metricproducer.GlobalManager().GetAll()
+	metrics := metricproducer.GlobalManager().GetAll()[0].Read()
+	for _, metric := range metrics {
+		if metric.Descriptor.Name == name {
+			return metric
+		}
+	}
+	return nil
+}
+
+// MetricToString convert metrics to a string. For test only
+func MetricToString(metric *metricdata.Metric) string {
+	var tsString []string
+	for _, ts := range metric.TimeSeries {
+		tsString = append(tsString, timeSeriesToString(ts))
+	}
+	sort.Strings(tsString)
+
+	var sb strings.Builder
+	name := metric.Descriptor.Name
+	sb.WriteString(name + "(")
+	for _, label := range metric.Descriptor.LabelKeys {
+		sb.WriteString(label.Key + ",")
+	}
+	sb.WriteString(") {\n")
+	sb.WriteString(strings.Join(tsString, "\n"))
+	sb.WriteString("\n}\n")
+	return sb.String()
+}
+
+func timeSeriesToString(ts *metricdata.TimeSeries) string {
+	var sb strings.Builder
+	sb.WriteString("  (")
+	for _, v := range ts.LabelValues {
+		sb.WriteString(v.Value + ",")
+	}
+	sb.WriteString(") : ")
+	sb.WriteString(fmt.Sprintf("%v", ts.Points[0].Value))
+	return sb.String()
+}
+
+// FindMetricDataPoint utility to find collected metrics by name and tag values for testing only
+func FindMetricDataPoint(name string, tagVal ...string) *metricdata.Point {
+	metric := FindMetrics(name)
+	for _, ts := range metric.TimeSeries {
+		if !tagValMatches(ts, tagVal...) {
+			continue
+		}
+		return &ts.Points[0]
+	}
+	return nil
+}
+
+func tagValMatches(ts *metricdata.TimeSeries, tagVal ...string) bool {
+	if len(ts.LabelValues) != len(tagVal) {
+		return false
+	}
+	for i, v := range ts.LabelValues {
+		if v.Value != tagVal[i] {
+			return false
+		}
+	}
+	return true
+}
