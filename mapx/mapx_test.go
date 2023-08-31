3dd9f10520de82d9dc6b722f1d303e1225ec1659
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2023-08-31 17:09:36 +0000

    Add mapx.Equals
    GitOrigin-RevId: 9786badd336823a60a57897c6dbad31e1363b758

---
+++ mapx/mapx_test.go
@@ -0,0 +1,29 @@
+package mapx_test
+
+import (
+	"go.atoms.co/lib/mapx"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestEquals(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		require.True(t, mapx.Equals(map[int]int{}, map[int]int{}))
+	})
+
+	t.Run("left empty", func(t *testing.T) {
+		require.False(t, mapx.Equals(map[int]int{}, map[int]int{1: 1}))
+	})
+
+	t.Run("right empty", func(t *testing.T) {
+		require.False(t, mapx.Equals(map[int]int{1: 1}, map[int]int{}))
+	})
+
+	t.Run("equal elements", func(t *testing.T) {
+		require.True(t, mapx.Equals(map[int]int{1: 1, 2: 2}, map[int]int{2: 2, 1: 1}))
+	})
+
+	t.Run("not equal elements", func(t *testing.T) {
+		require.False(t, mapx.Equals(map[int]int{1: 2, 2: 1}, map[int]int{2: 2, 1: 1}))
+	})
+}
