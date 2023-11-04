aaf3dd9b336c1e0f211343cc8330a51e64b3c890
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2023-11-03 18:39:44 -0700

    [libs/go] Replace some usages of chanx.Drain with chanx.Clear (#9361)
    `Drain` will changed to wait for channel closure.
    
    GitOrigin-RevId: 20083499b40357d15b2fbdf5147bb8b4806ccf62

--- chanx/chan_test.go
+++ chanx/chan_test.go
@@ -49,6 +49,46 @@ func TestDrain(t *testing.T) {
 	})
 }
 
+func TestClear(t *testing.T) {
+	t.Run("from empty channel", func(t *testing.T) {
+		ch := make(chan int)
+
+		chanx.Clear(ch)
+
+		require.Empty(t, ch)
+	})
+
+	t.Run("from closed channel", func(t *testing.T) {
+		ch := make(chan int)
+		close(ch)
+
+		chanx.Clear(ch)
+
+		require.Empty(t, ch)
+	})
+
+	t.Run("from non-empty channel", func(t *testing.T) {
+		ch := make(chan int, 5)
+		ch <- 1
+		ch <- 2
+
+		chanx.Clear(ch)
+
+		require.Empty(t, ch)
+	})
+
+	t.Run("from non-empty closed channel", func(t *testing.T) {
+		ch := make(chan int, 5)
+		ch <- 1
+		ch <- 2
+		close(ch)
+
+		chanx.Clear(ch)
+
+		require.Empty(t, ch)
+	})
+}
+
 func TestBreaker(t *testing.T) {
 	t.Run("chan", func(t *testing.T) {
 
