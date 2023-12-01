0eccc85461b21ca23932c5e7798aaf575b8ce354
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2023-12-01 10:23:09 -0800

    [KEQ2] Improve keq2 in local mode (#14318)
    This change removes the need to use keq2-topics image for local development. It adds `keq2dev` binary that starts keq2 with in-memory management database, similarly to start-single-node, but without configuration passed in CLI. Tenant configuration is loaded from the directory and keq2-topics syncer is used directly to initialize topics
    
    <REDACTED>
    
    GitOrigin-RevId: 38a717337fa3cc3f23c6c74f9b9fa03428ffb04f

---
+++ service/pprofx/pprofx.go
@@ -0,0 +1,35 @@
+package pprofx
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"go.cloudkitchens.org/lib/log"
+)
+
+type options struct {
+	port int
+}
+
+// Option is a test server option.
+type Option func(*options)
+
+func WithPort(port int) Option {
+	return func(o *options) {
+		o.port = port
+	}
+}
+
+// Start the default pprof handlers on the pprof port: https://golang.org/pkg/net/http/pprof/.
+func Start(ctx context.Context, opts ...Option) {
+	o := options{
+		port: 6060,
+	}
+	for _, fn := range opts {
+		fn(&o)
+	}
+
+	log.Infof(ctx, "Setting up pprof on port: %v", o.port)
+	log.Fatal(ctx, http.ListenAndServe(fmt.Sprintf(":%v", o.port), nil))
+}
