a30708a79a0a9715a967f48ed31803db2160ce21
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2024-03-21 16:18:02 -0700

    [KEQ2] Handle dirty scheduled messages (#32642)
    When schedule or cancel schedule operations fail, the scheduler needs to re-read the messages. This change introduces dirty schedules that are read individually and added back if needed.
    
    Cancelled messages are now removed from loaded messages on I/O failures and marked as dirty. They are added back if dirty read discovers that the message was not cancelled.
    
    Messages are marked as dirty only if they are in the horizon; otherwise they will be discovered during regular scan.
    
    GitOrigin-RevId: 078df536c0d77848319f60d6fbfaa511d62a93ec

--- testing/requirex/chan.go
+++ testing/requirex/chan.go
@@ -5,16 +5,34 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+
+	"go.cloudkitchens.org/lib/chanx"
 )
 
-// ChanEmpty verifies that the channel is empty. Fails if the channel is not empty after given duration
-func ChanEmpty[T any](t *testing.T, c <-chan T, limit time.Duration) {
+const (
+	chanWait = 2 * time.Second
+)
+
+// ChanEmpty verifies that the channel is empty. Fails if the channel is not empty after some time
+func ChanEmpty[T any](t *testing.T, c <-chan T) {
 	t.Helper()
-	require.Eventually(t, func() bool { return len(c) == 0 }, limit, 100*time.Millisecond)
+	require.Eventually(t, func() bool { return len(c) == 0 }, chanWait, 100*time.Millisecond)
 }
 
-// ChanNonEmpty verifies that the channel has elements. Fails if the channel is empty after given duration
-func ChanNonEmpty[T any](t *testing.T, c <-chan T, limit time.Duration) {
+// ChanNonEmpty verifies that the channel has elements. Fails if the channel is empty after some time
+func ChanNonEmpty[T any](t *testing.T, c <-chan T) {
 	t.Helper()
-	require.Eventually(t, func() bool { return len(c) > 0 }, limit, 100*time.Millisecond)
+	require.Eventually(t, func() bool { return len(c) > 0 }, chanWait, 100*time.Millisecond)
+}
+
+// Element requires an element in the given channel within a grace period.
+func Element[T any](t *testing.T, ch <-chan T, args ...any) T {
+	t.Helper()
+
+	elm, ok := chanx.TryRead(ch, chanWait)
+	if len(args) == 0 {
+		args = []any{"no element in channel"}
+	}
+	require.True(t, ok, args...)
+	return elm
 }
