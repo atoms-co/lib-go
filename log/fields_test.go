5382cbcc27f757e47d8b384634f109f1c3fb518d
Kasper Føns <kasper.fons@cloudkitchens.com>
2024-08-14 23:07:25 +0200

    [log] Fix NewContext overwriting fields of other contexts (#55874)
    Summary:
    - Since the slice is a view on top of a backing array, appending to can overwrite elements in the backing array of other slices.
    - Copy the slice contents instead
    - Ref: https://go.dev/play/p/1BRWOxmXaoU
    - Ref: https://github.com/golang/go/issues/28780
    GitOrigin-RevId: 84abe7258ea709d51de5e9a07aeeffd0ad55d446

---
+++ log/fields_test.go
@@ -0,0 +1,34 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.atoms.co/lib/log"
+)
+
+func Test_Given_Context_When_TwoNewChildContexts_Then_FieldsAreNotOverwritten(t *testing.T) {
+	var ctx1Fields []log.Field
+	var ctx2Fields []log.Field
+	var parentFields []log.Field
+	for i := 0; i < 12; i++ {
+		parentFields = append(parentFields, log.Int(fmt.Sprintf("parent-%d", i), i))
+		ctx1Fields = append(ctx1Fields, log.Int(fmt.Sprintf("parent-%d", i), i))
+		ctx2Fields = append(ctx2Fields, log.Int(fmt.Sprintf("parent-%d", i), i))
+	}
+
+	parent := log.NewContext(context.Background(), parentFields...)
+
+	ctx1 := log.NewContext(parent, log.String("child1", "child1-value"))
+	ctx2 := log.NewContext(parent, log.String("child2", "child2-value"))
+
+	ctx1Fields = append(ctx1Fields, log.String("child1", "child1-value"))
+	ctx2Fields = append(ctx2Fields, log.String("child2", "child2-value"))
+
+	assert.Equal(t, parentFields, log.FromContext(parent))
+	assert.Equal(t, ctx1Fields, log.FromContext(ctx1))
+	assert.Equal(t, ctx2Fields, log.FromContext(ctx2))
+}
