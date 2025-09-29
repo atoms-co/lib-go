fab2b444da5d937a1d5d980e887fcb5964f963de
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2025-09-29 09:49:13 -0700

    [libs/go] Add css.com/lib-go/pkg/encoding/protox library (#109832)
    Adding a new library to work with protobuf that use `google.golang.org/protobuf` package instead of deprecated `github.com/golang/protobuf`.
    
    In contains some helper methods to make transition easier. Also contains custom helper `Unmarshal`.
    
    GitOrigin-RevId: 6568fac0b86a8142163ad3abec89387e8e94a3a9

---
+++ encoding/protox/proto_test.go
@@ -0,0 +1,75 @@
+package protox_test
+
+import (
+	"strings"
+	"testing"
+
+	legacyproto "github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.cloudkitchens.org/lib/encoding/protox"
+	"go.cloudkitchens.org/lib/testing/requirex"
+)
+
+func TestUnmarshal(t *testing.T) {
+	buf, err := proto.Marshal(&timestamppb.Timestamp{
+		Seconds: 123,
+		Nanos:   456,
+	})
+	require.NoError(t, err)
+
+	ts, err := protox.Unmarshal[timestamppb.Timestamp](buf)
+	require.NoError(t, err)
+	requirex.Equal(t, ts.Seconds, 123)
+	requirex.Equal(t, ts.Nanos, 456)
+}
+
+func TestUnmarshalLegacy(t *testing.T) {
+	buf, err := legacyproto.Marshal(&timestamppb.Timestamp{
+		Seconds: 123,
+		Nanos:   456,
+	})
+	require.NoError(t, err)
+
+	ts, err := protox.Unmarshal[timestamppb.Timestamp](buf)
+	require.NoError(t, err)
+	requirex.Equal(t, ts.Seconds, 123)
+	requirex.Equal(t, ts.Nanos, 456)
+}
+
+func TestCompactTextString(t *testing.T) {
+	pb := &timestamppb.Timestamp{
+		Seconds: 123,
+		Nanos:   456,
+	}
+
+	str := protox.CompactTextString(pb)
+	// Need to reduce double whitespaces, prototext.encode adds randomly whitespace between fields
+	requirex.Equal(t, strings.Replace(str, "  ", " ", -1), "seconds:123 nanos:456")
+}
+
+func TestMarshalTextString(t *testing.T) {
+	pb := &timestamppb.Timestamp{
+		Seconds: 123,
+		Nanos:   456,
+	}
+
+	str := protox.MarshalTextString(pb)
+	// Need to reduce double whitespaces, prototext.encode adds randomly whitespace between fields
+	requirex.Equal(t, strings.Replace(str, "  ", " ", -1), `seconds: 123
+nanos: 456
+`)
+}
+
+func TestClone(t *testing.T) {
+	pb := &timestamppb.Timestamp{
+		Seconds: 123,
+		Nanos:   456,
+	}
+
+	ts := protox.Clone(pb)
+	requirex.Equal(t, ts.Seconds, 123)
+	requirex.Equal(t, ts.Nanos, 456)
+}
