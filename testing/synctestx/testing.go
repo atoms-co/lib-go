b6045f21df9112e818979aef1dfe9955d0944964
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2025-11-06 16:19:36 -0800

    Add synctestx.Run
    GitOrigin-RevId: cfe0d3027b5c7921b7cb93aa1e5db755ec4e346c

---
+++ testing/synctestx/testing.go
@@ -0,0 +1,16 @@
+package synctestx
+
+import (
+	"testing"
+	"testing/synctest"
+)
+
+// Run executes a test in synctest context
+func Run(t *testing.T, name string, f func(t *testing.T)) {
+	t.Helper()
+	t.Run(name, func(t *testing.T) {
+		synctest.Run(func() {
+			f(t)
+		})
+	})
+}
