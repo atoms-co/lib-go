e592b718a86b2bcb507e6d313971b1e0d4324a5e
Sergey Tyurin <sergey.tyurin@cloudkitchens.com>
2026-04-09 17:00:58 -0700

    Build with go 1.25 (#2)

--- testing/synctestx/testing.go
+++ testing/synctestx/testing.go
@@ -1,16 +1,9 @@
+//go:build go1.25
+
 package synctestx
 
 import (
-	"testing"
 	"testing/synctest"
 )
 
-// Run executes a test in synctest context
-func Run(t *testing.T, name string, f func(t *testing.T)) {
-	t.Helper()
-	t.Run(name, func(t *testing.T) {
-		synctest.Run(func() {
-			f(t)
-		})
-	})
-}
+var Run = synctest.Test
