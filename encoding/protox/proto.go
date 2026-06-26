179a0fe5d85ece79f2738a5bdacaa3d137a3e999
xuhui-liu_css <xuhui.liu@cloudkitchens.com>
2026-06-26 15:10:38 -0700

    add Size

--- encoding/protox/proto.go
+++ encoding/protox/proto.go
@@ -49,6 +49,13 @@ func MarshalTextString(m proto.Message) string {
 	return prototext.Format(m)
 }
 
+// Size returns the size in bytes of the wire-format encoding of m.
+// It is a wrapper of proto.Size()
+func Size(m proto.Message) int {
+	return proto.Size(m)
+}
+
+
 // Clone returns a deep copy of m. If the top-level message is invalid,
 // it returns an invalid message as well.
 func Clone[M proto.Message](m M) M {
